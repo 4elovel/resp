4ebff0afba7350017edf5a7a7f36df91cce913a0
4elovel <marcynakvova007@gmail.com>
2023-11-07 19:38:23 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Text;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://philpapers.org/");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        Console.WriteLine("Сайт має заголовки:");
+        for (int i = 0; i < response.Headers.Count; ++i)
+            Console.WriteLine("\nHeader Name: {0}, Value : {1}", response.Headers.Keys[i], response.Headers[i]);
+
+    }
+}
